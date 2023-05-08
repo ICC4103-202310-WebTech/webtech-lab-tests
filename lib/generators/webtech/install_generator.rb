caa824ce7120e7895c1c71331ff7053e51e0ef60
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2023-05-08 11:43:32 -0400

    feat: complet test models
    - add fixture items
    - add missing test item and restaurant

--- lib/generators/webtech/install_generator.rb
+++ lib/generators/webtech/install_generator.rb
@@ -3,19 +3,19 @@
 require 'rails/generators/base'
 
 module Webtech
-    class InstallGenerator < Rails::Generators::Base
-        source_root File.expand_path('templates', __dir__ )
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('templates', __dir__)
 
-        def copy_fixtures
-            copy_file 'fixtures/dishes.yml', 'test/fixtures/categories.yml'
-            copy_file 'fixtures/items.yml', 'test/fixtures/items.yml'
-            copy_file 'fixtures/restaurants.yml', 'test/fixtures/restaurants.yml'
-        end
-
-        def copy_model_tests
-            copy_file 'models/dish_test.rb', 'test/models/dish_test.rb'
-            copy_file 'models/item_test.rb', 'test/models/item_test.rb'
-            copy_file 'models/restaurant_test.rb', 'test/models/restaurant_test.rb'
-        end
+    def copy_fixtures
+      copy_file 'fixtures/dishes.yml', 'test/fixtures/dishes.yml'
+      copy_file 'fixtures/items.yml', 'test/fixtures/items.yml'
+      copy_file 'fixtures/restaurants.yml', 'test/fixtures/restaurants.yml'
     end
+
+    def copy_model_tests
+      copy_file 'models/dish_test.rb', 'test/models/dish_test.rb'
+      copy_file 'models/item_test.rb', 'test/models/item_test.rb'
+      copy_file 'models/restaurant_test.rb', 'test/models/restaurant_test.rb'
+    end
+  end
 end
