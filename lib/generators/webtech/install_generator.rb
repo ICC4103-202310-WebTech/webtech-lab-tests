74d0ebb9c2caacfeeecef3deedcebe6d4bf08de9
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2023-05-08 11:51:53 -0400

    feat: add test routes

--- lib/generators/webtech/install_generator.rb
+++ lib/generators/webtech/install_generator.rb
@@ -5,7 +5,6 @@ require 'rails/generators/base'
 module Webtech
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
-
     def copy_fixtures
       copy_file 'fixtures/dishes.yml', 'test/fixtures/dishes.yml'
       copy_file 'fixtures/items.yml', 'test/fixtures/items.yml'
@@ -16,6 +15,7 @@ module Webtech
       copy_file 'models/dish_test.rb', 'test/models/dish_test.rb'
       copy_file 'models/item_test.rb', 'test/models/item_test.rb'
       copy_file 'models/restaurant_test.rb', 'test/models/restaurant_test.rb'
+      copy_file 'integration/routes_test.rb', 'test/integration/routes_test.rb'
     end
   end
 end
